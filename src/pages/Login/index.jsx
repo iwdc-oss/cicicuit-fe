1702a6ac9261025932917ad9517b2defb87f904f
Didul-arch <syadidulazmi11@gmail.com>
2025-09-11 18:59:45 +0700

    added font and login button

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,23 +1,41 @@
-import images from '@/utils/images.js'
+import images from '@/utils/images.js';
 
 const LoginPage = () => {
   return (
     <>
       <div className='flex justify-center items-center mt-52 mx-auto flex-col max-w-[480px] gap-14'>
-        <img src={images.LOGO.LOGO_CICICUIT} alt="logo brand" className='max-w-96'/>
+        <img
+          src={images.LOGO.LOGO_CICICUIT}
+          alt='logo brand'
+          className='max-w-96'
+        />
         <div className='flex self-start w-full'>
           <form action='' method='post' className='flex flex-col gap-4 w-full'>
             <div className='text-sm w-full'>
-              <input type="text" placeholder='Username' className='border-1 border-slate-400 p-3 rounded-md w-full' />
+              <input
+                type='text'
+                placeholder='Username'
+                className='border-1 border-slate-400 p-3 rounded-md w-full'
+              />
             </div>
             <div className='text-sm w-full'>
-              <input type="password" placeholder='Password' className='border-1 border-slate-400 p-3 rounded-md w-full' />
+              <input
+                type='password'
+                placeholder='Password'
+                className='border-1 border-slate-400 p-3 rounded-md w-full'
+              />
             </div>
+            <button
+              type='submit'
+              className='bg-black text-white py-3 rounded-md disabled:opacity-60'
+            >
+              Login
+            </button>
           </form>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default LoginPage;
